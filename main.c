44beac34d270659410976103b2e57246387e315b
Ariz Azadli <arizazadli20@gmail.com>
2026-06-10 01:09:04 +0400

    variables

--- main.c
+++ main.c
@@ -1,22 +1,19 @@
-/**
-* "Hello world!"  must be the very first program in any
- * introductory programming module. That is a rule, sorry.
- */
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
 
 int main() {
 
-    int age = 10;
-    float gpa = 1.38;
-    char grade = 'A';
-    char youtube[] = "Ariz Azadov";
+  bool isonline=1;
 
-    printf("I am %d\n",age);
-    printf("My gpa is %f\n",gpa);
-    printf("My grades are %c\n",grade);
-    printf("My youtube chanel is %s\n",youtube);
+    if (isonline) {
+        printf("You are Online\n");
+    }
+
+else {
+    printf("You are Ofline\n");
+}
 
     return 0;
 }
