16d05f32b2d8a7ef04e9a35b31bfba1f67e7f42a
Ariz Azadli <arizazadli20@gmail.com>
2026-06-10 00:40:03 +0400

    variables

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
+/**
+* "Hello world!"  must be the very first program in any
+ * introductory programming module. That is a rule, sorry.
+ */
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, Niggas!\n");
+
+int main() {
+
+    int age = 10;
+    float gpa = 1.38;
+    char grade = 'A';
+    char youtube[] = "Ariz Azadov";
+
+    printf("I am %d\n",age);
+    printf("My gpa is %f\n",gpa);
+    printf("My grades are %c\n",grade);
+    printf("My youtube chanel is %s\n",youtube);
+
     return 0;
 }
