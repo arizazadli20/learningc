a0b56a1a61442c752e3d3e8bad15441c4f1a408a
Ariz Azadli <arizazadli20@gmail.com>
2026-06-11 19:05:38 +0400

    locker game

---
+++ locket.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <string.h>
+
+//Locker
+
+int main() {
+
+    int first=0;
+    int second=0;
+    int third=0;
+    int fourth=0;
+
+    printf("Enter the first digit: ");
+    scanf("%d", &first); // Rəqəm alırıq
+
+    if (first == 2) { printf("Enter the second digit: ");}
+    else {printf("Failed");}
+    scanf("%d", &second);
+
+    if (second == 0) { printf("Enter the third digit: ");}
+    else {printf("Failed");}
+    scanf("%d", &third);
+
+    if (third == 0) { printf("Enter the fourth digit: ");}
+    else {printf("Failed");}
+    scanf("%d", &fourth);
+
+    if (fourth == 8) { printf("SUCCESSFULL, Your code is %d%d%d%d", first,second,third,fourth); }
+    else {printf("Failed");}
+
+
+
+    return 0;
+}
+//
+// Created by ARIZ AZADOV on 11/06/26.
+//
