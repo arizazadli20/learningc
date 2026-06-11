1c35aa20df5976f6f125bf338693ae35ecd51fed
Ariz Azadli <arizazadli20@gmail.com>
2026-06-11 17:41:59 +0400

    new game

--- main.c
+++ main.c
@@ -6,10 +6,23 @@
 int main() {
 
     char item[50] = "";
+    char currency = '$';
     float price=0;
     int quantity=0;
+    float total=0;
 
     printf("Which product u want to buy?");
+    scanf("%s",item);
+
+    printf("What is the price of product?:  ");
+    scanf("%f",&price);
+
+    printf("What is the quantity?:  ");
+    scanf(" %d",&quantity);
+
+    total=price*quantity;
+
+    printf("%c""%.1f",currency,total);
 
 
     return 0;
