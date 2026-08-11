f7fdecd216a38d8c8bb174679111c084e708afc3
Ariz Azadli <arizazadli20@gmail.com>
2026-08-11 16:12:11 +0400

    homework 01 practise for new semester

--- main.c
+++ main.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
+#include <math.h>
 
-//task1
+//homework task 1
 
-int main () {
+int main() {
 
-    int a=0;
+    double x1=0.0;
+    double y1=0.0;
+    double r1=0.0;
+    double x2=0.0;
+    double y2=0.0;
+    double r2=0.0;
 
-    printf("ml' nob:\n");
-    if (scanf("%d", &a) != 1) {
-        printf("Neh mi'\n");
+   
+    printf("Enter circle #1 parameters:\n");
+    if (scanf("%lf %lf %lf", &x1, &y1, &r1) != 3 || r1 <= 0) {
+        printf("Invalid input.\n");
         return 0;
     }
 
-    if (a<0 || a>8) {
-        printf("Qih mi' %d\n", a);
+    printf("Enter circle #2 parameters:\n");
+    if (scanf("%lf %lf %lf", &x2, &y2, &r2) != 3 || r2 <= 0) {
+        printf("Invalid input.\n");
+        return 0;
     }
 
 
-    else {
-        printf("Qapla'\n");
-         if(a==0) { printf("noH QapmeH wo' Qaw'lu'chugh yay chavbe'lu' 'ej wo' choqmeH may' DoHlu'chugh lujbe'lu'.\n");}
-            else if (a==1) {printf("bortaS bIr jablu'DI' reH QaQqu' nay'.\n");}
-            else if (a==2) {printf("Qu' buSHa'chugh SuvwI', batlhHa' vangchugh, qoj matlhHa'chugh, pagh ghaH SuvwI''e'.\n");}
-            else if (a==3) {printf("bISeH'eghlaH'be'chugh latlh Dara'laH'be'.\n");}
-            else if (a==4) {printf("qaStaHvIS wa' ram loS SaD Hugh SIjlaH qetbogh loD.\n");}
-            else if (a==5) {printf("Suvlu'taHvIS yapbe' HoS neH.\n");}
-            else if (a==6) {printf("Ha'DIbaH DaSop 'e' DaHechbe'chugh yIHoHQo'.\n");}
-            else if (a==7) {printf("Heghlu'meH QaQ jajvam.\n");}
-            else if (a==8) {printf("leghlaHchu'be'chugh mIn lo'laHbe' taj jej.\n");}
+    double d = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
 
-        }
 
-    }
+
+
+    return 0;
+}
